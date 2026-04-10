12073e9adba356c6388520380db1c69e4f863d63
Awais-Naveed <201934757+Awais-Naveed@users.noreply.github.com>
2026-04-10 09:28:32 +0100

    skeletal journey up to confirmation

--- src/test/scala/uk/gov/hmrc/perftests/cis/requests/StandardMonthlyReturnRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/cis/requests/StandardMonthlyReturnRequests.scala
@@ -187,4 +187,272 @@ object StandardMonthlyReturnRequests extends ServicesConfiguration with CisPerfo
       .formParam("csrfToken", f"#{csrfToken}")
       .check(status.is(303))
 
+  val getYouHaveAddedDetailsForASingleSubcontractor: HttpRequestBuilder =
+    http("[get ] You have added details for a single subcontractor page")
+      .get(cisFrontendUrl + "/monthly-return/subcontractor-details-added")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postYouHaveAddedDetailsForASingleSubcontractor(option: String): HttpRequestBuilder =
+    http("[post] You have added details for a single subcontractor page")
+      .post(cisFrontendUrl + "/monthly-return/subcontractor-details-added")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getWhichSubcontractorDoYouNeedToAddPaymentDetailsFor: HttpRequestBuilder =
+    http("[get ] Which subcontractor do you need to add payment details for page")
+      .get(cisFrontendUrl + "/monthly-return/subcontractor-details-added")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postWhichSubcontractorDoYouNeedToAddPaymentDetailsFor(option: String): HttpRequestBuilder =
+    http("[post] Which subcontractor do you need to add payment details for page")
+      .post(cisFrontendUrl + "/monthly-return/subcontractor-details-added")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getHowMuchDidYouPayToSecondSubcontractorInTotalPage: HttpRequestBuilder =
+    http("[get ] How much did you pay to Two Subcontractor in total?")
+      .get(cisFrontendUrl + "/monthly-return/payment-details/2")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postHowMuchDidYouPayToSecondSubcontractorInTotalPage(amount: String): HttpRequestBuilder =
+    http("[post] How much did you pay to Two Subcontractor in total?")
+      .post(cisFrontendUrl + "/monthly-return/payment-details/2")
+      .formParam("value", amount)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getHowMuchDidSecondSubcontractorPayInMaterialCosts: HttpRequestBuilder =
+    http("[get ] How much did Two Subcontractor pay in material costs?")
+      .get(cisFrontendUrl + "/monthly-return/materials-cost/2")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postHowMuchDidSecondSubcontractorPayInMaterialCosts(amount: String): HttpRequestBuilder =
+    http("[post] How much did Two Subcontractor pay in material costs?")
+      .post(cisFrontendUrl + "/monthly-return/materials-cost/2")
+      .formParam("value", amount)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getHowMuchTaxInTotalDidYouDeductFromSecondSubcontractor: HttpRequestBuilder =
+    http("[get ] How much tax in total did you deduct from Two Subcontractor?")
+      .get(cisFrontendUrl + "/monthly-return/tax-deducted/2")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postHowMuchTaxInTotalDidYouDeductFromSecondSubcontractor(amount: String): HttpRequestBuilder =
+    http("[post] How much tax in total did you deduct from Two Subcontractor?")
+      .post(cisFrontendUrl + "/monthly-return/tax-deducted/2")
+      .formParam("value", amount)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getCheckYourAnswersForSecondSubcontractor: HttpRequestBuilder =
+    http("[get ] Check your answers for Two Subcontractor")
+      .get(cisFrontendUrl + "/monthly-return/check-answers-total-payments/2")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  val postCheckYourAnswersForSecondSubcontractor: HttpRequestBuilder =
+    http("[post] Check your answers for Two Subcontractor")
+      .post(cisFrontendUrl + "/monthly-return/check-answers-total-payments/2")
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getYouHaveAddedDetailsFor2Subcontractors: HttpRequestBuilder =
+    http("[get ] You have added details for 2 subcontractors page")
+      .get(cisFrontendUrl + "/monthly-return/subcontractor-details-added")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postYouHaveAddedDetailsFor2Subcontractors(option: String): HttpRequestBuilder =
+    http("[post] You have added details for 2 subcontractors page")
+      .post(cisFrontendUrl + "/monthly-return/subcontractor-details-added")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getAreYouSureYouWantToRemoveFirstSubcontractor: HttpRequestBuilder =
+    http("[get ] Are you sure you want to remove One Subcontractor page")
+      .get(cisFrontendUrl + "/monthly-return/change-remove-subcontractor/1")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postAreYouSureYouWantToRemoveFirstSubcontractor(option: String): HttpRequestBuilder =
+    http("[post] Are you sure you want to remove One Subcontractor page")
+      .post(cisFrontendUrl + "/monthly-return/change-remove-subcontractor/1")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getSummaryOfPaymentsMade: HttpRequestBuilder =
+    http("[get ] Summary of payments made page")
+      .get(cisFrontendUrl + "/monthly-return/summary-subcontractor-payments")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  val postSummaryOfPaymentsMade: HttpRequestBuilder =
+    http("[post] Summary of payments made page")
+      .post(cisFrontendUrl + "/monthly-return/summary-subcontractor-payments")
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getDoYouConfirmTheInformationInThisReturnIsCorrect: HttpRequestBuilder =
+    http("[get ] Do you confirm the information in this return is correct page")
+      .get(cisFrontendUrl + "/monthly-return/payment-details-confirmation")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postDoYouConfirmTheInformationInThisReturnIsCorrect(option: String): HttpRequestBuilder =
+    http("[post] Do you confirm the information in this return is correct page")
+      .post(cisFrontendUrl + "/monthly-return/payment-details-confirmation")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getDeclarationOfEmploymentStatus: HttpRequestBuilder =
+    http("[get ] Declaration of employment status page")
+      .get(cisFrontendUrl + "/monthly-return/employment-status-declaration")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postDeclarationOfEmploymentStatus(option: String): HttpRequestBuilder =
+    http("[post] Declaration of employment status page")
+      .post(cisFrontendUrl + "/monthly-return/employment-status-declaration")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getDeclarationOfVerifiedStatus: HttpRequestBuilder =
+    http("[get ] Declaration of verified status page")
+      .get(cisFrontendUrl + "/monthly-return/verified-status-declaration")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postDeclarationOfVerifiedStatus(option: String): HttpRequestBuilder =
+    http("[post] Declaration of verified status page")
+      .post(cisFrontendUrl + "/monthly-return/verified-status-declaration")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getSubmitInactivityRequest: HttpRequestBuilder =
+    http("[get ] Submit inactivity request page")
+      .get(cisFrontendUrl + "/monthly-return/submit-inactivity-request")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postSubmitInactivityRequest(option: String): HttpRequestBuilder =
+    http("[post] Submit inactivity request page")
+      .post(cisFrontendUrl + "/monthly-return/submit-inactivity-request")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getDoYouWantConfirmationByEmailThatThisReturnHasBeenSuccessfullySubmitted: HttpRequestBuilder =
+    http("[get ] Do you want confirmation by email that this return has been successfully submitted page")
+      .get(cisFrontendUrl + "/monthly-return/do-you-want-email-confirmation")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postDoYouWantConfirmationByEmailThatThisReturnHasBeenSuccessfullySubmitted(option: String): HttpRequestBuilder =
+    http("[post] Do you want confirmation by email that this return has been successfully submitted page")
+      .post(cisFrontendUrl + "/monthly-return/do-you-want-email-confirmation")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getCheckYourAnswersBeforeSubmittingYourReturn: HttpRequestBuilder =
+    http("[get ] Check your answers before submitting your return page")
+      .get(cisFrontendUrl + "/monthly-return/check-your-answers")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  val postCheckYourAnswersBeforeSubmittingYourReturn: HttpRequestBuilder =
+    http("[post] Check your answers before submitting your return page")
+      .post(cisFrontendUrl + "/monthly-return/check-your-answers")
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getChangeDeclarationOfEmploymentStatus: HttpRequestBuilder =
+    http("[get ] Change declaration of employment status page")
+      .get(cisFrontendUrl + "/monthly-return/change-employment-status-declaration")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postChangeDeclarationOfEmploymentStatus(option: String): HttpRequestBuilder =
+    http("[post] Change declaration of employment status page")
+      .post(cisFrontendUrl + "/monthly-return/change-employment-status-declaration")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getChangeDeclarationOfVerifiedStatus: HttpRequestBuilder =
+    http("[get ] Change declaration of verified status page)")
+      .get(cisFrontendUrl + "/monthly-return/change-verified-status-declaration")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postChangeDeclarationOfVerifiedStatus(option: String): HttpRequestBuilder =
+    http("[post] Change declaration of verified status page")
+      .post(cisFrontendUrl + "/monthly-return/change-verified-status-declaration")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getChangeSubmitInactivityRequest: HttpRequestBuilder =
+    http("[get ] Change submit inactivity request page")
+      .get(cisFrontendUrl + "/monthly-return/change-submit-inactivity-request")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postChangeSubmitInactivityRequest(option: String): HttpRequestBuilder =
+    http("[post] Change submit inactivity request page")
+      .post(cisFrontendUrl + "/monthly-return/change-submit-inactivity-request")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getInactivityRequest: HttpRequestBuilder =
+    http("[get ] Inactivity request page")
+      .get(cisFrontendUrl + "/monthly-return/change-inactivity-request-warning")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  val postInactivityRequest: HttpRequestBuilder =
+    http("[post] Inactivity request page")
+      .post(cisFrontendUrl + "/monthly-return/change-inactivity-request-warning")
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getChangeDoYouWantConfirmationByEmailThatThisReturnHasBeenSuccessfullySubmitted: HttpRequestBuilder =
+    http("[get ] Change do you want confirmation by email that this return has been successfully submitted page")
+      .get(cisFrontendUrl + "/monthly-return/change-do-you-want-email-confirmation")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postChangeDoYouWantConfirmationByEmailThatThisReturnHasBeenSuccessfullySubmitted(option: String): HttpRequestBuilder =
+    http("[post] Change do you want confirmation by email that this return has been successfully submitted page")
+      .post(cisFrontendUrl + "/monthly-return/change-do-you-want-email-confirmation")
+      .formParam("value", option)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
+
+  val getEnterYourEmailAddress: HttpRequestBuilder =
+    http("[get ] Enter your email address page")
+      .get(cisFrontendUrl + "/monthly-return/change-enter-your-email-address")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  def postEnterYourEmailAddress(email: String): HttpRequestBuilder =
+    http("[post] Enter your email address page")
+      .post(cisFrontendUrl + "/monthly-return/enter-your-email-address")
+      .formParam("value", email)
+      .formParam("csrfToken", f"#{csrfToken}")
+      .check(status.is(303))
 }
